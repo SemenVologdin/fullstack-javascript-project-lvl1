3a0f31481176aea3614bfee22bb1ad64a1521f83
Vologdin Semen <vologdin.dev@gmail.com>
2023-01-05 20:22:35 +0500

    Fix

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -22,9 +22,14 @@ for( let i = 0; i < tryCount; i++ ){
     }
 }
 
-function isPrime(num){
-    for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
-        if(num % i === 0) return false;
-    }
-    return num > 1;
-}
+function isPrime(num) {
+    var sqrtnum=Math.floor(Math.sqrt(num));
+      var prime = num != 1;
+      for(var i=2; i<sqrtnum+1; i++) {
+          if(num % i == 0) {
+              prime = false;
+              break;
+          }
+      }
+      return prime;
+  }
