8eb4ce813d9cedcd6702bf75e34c383168dc7768
Vologdin Semen <vologdin.dev@gmail.com>
2023-01-05 17:27:31 +0500

    Fix brain even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,12 @@
 import readlineSync from 'readline-sync'
 
+import {cli} from '../src/cli.js'
+
+const name = cli()
 console.log('Answer "yes" if the number is even, otherwise answer "no"')
 
-for( let i = 0; i < 3; i++ ){
+const tryCount = 3
+for( let i = 0; i < tryCount; i++ ){
     let randomNumber = Math.ceil(Math.random() * 10);
     let isEven = randomNumber % 2 == 0;
     let answer = readlineSync.question(`Question: ${randomNumber} `);
@@ -10,8 +14,12 @@ for( let i = 0; i < 3; i++ ){
 
     if( answer == correctAnswer ){
         console.log("Correct!")
+        if( i == tryCount - 1 ){
+            console.log(`Good job, ${name}`)
+        }
     }else{
         console.log(`"${answer}" is wrong answer :(. Correct answer was "${correctAnswer}"`)
         break;
     }
 }
+
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,9 @@
 import readlineSync from 'readline-sync'
 
-console.log('Welcome!')
-const name = readlineSync.question('What\'s your name?: ')
-console.log('Hello', name)
+export function cli() {
+    console.log('Welcome!')
+    const name = readlineSync.question('What\'s your name?: ')
+    console.log('Hello', name)
+    
+    return name
+}
